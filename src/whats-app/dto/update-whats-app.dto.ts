e624b22f46a01a8f6589175cb7ac59f1b1198302
Hagave <hagavepro@gmail.com>
2025-08-08 11:41:07 -0300

    removing extra files on main branch

--- src/whats-app/dto/update-whats-app.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateWhatsAppDto } from './create-whats-app.dto';
-
-export class UpdateWhatsAppDto extends PartialType(CreateWhatsAppDto) {}
